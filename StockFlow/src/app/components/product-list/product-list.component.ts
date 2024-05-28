cc68cd3eba18f99748306dd7b576ccaf79ffdf29
Victor Quirino <victorsouzaq@gmail.com>
2024-05-27 23:31:56 -0300

    feat - listagem de produto

--- StockFlow/src/app/components/product-list/product-list.component.ts
+++ StockFlow/src/app/components/product-list/product-list.component.ts
@@ -1,18 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Product } from '../../models/product.model';
 import { CommonModule } from '@angular/common';
 import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
+import { DatabaseService } from '../../services/database.service';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [CommonModule, MatTableModule, MatButtonModule, RouterLink],
+  imports: [CommonModule, MatTableModule, MatButtonModule, RouterLink, MatIconModule],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
   products: Product[] = [];
+  displayedColumns: string[] = ['name', 'description', 'quantity', 'category', 'entryDate', 'exitDate', 'actions'];
 
+  constructor(private dbService: DatabaseService) { }
+
+  ngOnInit(): void {
+    this.dbService.getProducts().subscribe(response => {
+      this.products = response;
+      console.log(this.products);
+    });
+  }
+
+  deleteProduct(id: string) {
+    //this.dbService.deleteProduct(id);
+  }
 }
--- StockFlow/src/app/services/database.service.ts
+++ StockFlow/src/app/services/database.service.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Product } from '../models/product.model';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -14,5 +15,24 @@ export class DatabaseService {
     this.http.post(`${this.API_URL}/products.json`, product).subscribe(response => {
       console.log(response);
     });
+  }
+
+  getProducts() {
+    return this.http.get<{ [key: string]: Product }>(`${this.API_URL}/products.json`,
+      {
+        params: new HttpParams().set('print', 'pretty')
+      }
+    )
+    .pipe(
+      map((responseData) => {
+        const array: Product[] = [];
+        for( const key in responseData) {
+          if((responseData).hasOwnProperty(key)) {
+            array.push({...(responseData as any)[key], id: key});
+          }
+        }
+        return array;
+      })
+    )
   }
 }
