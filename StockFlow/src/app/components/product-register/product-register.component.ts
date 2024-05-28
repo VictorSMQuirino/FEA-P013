55d4a28145fae5fd65d00bcdf0b4fb7737f92466
Victor Quirino <victorsouzaq@gmail.com>
2024-05-27 22:26:11 -0300

    feat - cadastro de produto

--- StockFlow/src/app/components/product-register/product-register.component.ts
+++ StockFlow/src/app/components/product-register/product-register.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { DatabaseService } from '../../services/database.service';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
@@ -8,6 +8,9 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { provideNativeDateAdapter } from '@angular/material/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { Router, RouterLink } from '@angular/router';
+import { Product } from '../../models/product.model';
 
 @Component({
   selector: 'app-product-register',
@@ -21,26 +24,40 @@ import { CommonModule } from '@angular/common';
     MatFormFieldModule,
     MatSelectModule,
     MatDatepickerModule,
+    MatButtonModule,
+    RouterLink,
     CommonModule,
   ],
   templateUrl: './product-register.component.html',
   styleUrl: './product-register.component.css'
 })
-export class ProductRegisterComponent {
+export class ProductRegisterComponent implements OnInit {
   productForm: FormGroup;
 
-  categories: string[] = ['Eletrônicos', 'Roupas e acessórios', 'Alimentação'];
+  categories: string[] = ['Eletrônicos', 'Roupas e acessórios', 'Alimentação', 'Móveis e decoração'];
 
-  constructor(private dbService: DatabaseService) {
-    this.productForm = new FormGroup({
-      'productName': new FormControl(null, [Validators.required]),
-      'productDescription': new FormControl(null, [Validators.required]),
-      'productQuantity': new FormControl(null, [Validators.required, this.productQuantityValidator.bind(this)]),
-      'productCategory': new FormControl(null, [Validators.required]),
-      'entryDate': new FormControl(null, [Validators.required]),
-      'exitDate': new FormControl(null, [Validators.required])
-    });
+  constructor(private dbService: DatabaseService, private formBuilder: FormBuilder, private router: Router) {
+    this.productForm = this.formBuilder.group({
+      productName: [null, [Validators.required]],
+      productDescription: [null, [Validators.required]],
+      productQuantity: [null, [Validators.required, this.productQuantityValidator.bind(this)]],
+      productCategory: [null, [Validators.required]],
+      entryDate: [null, [Validators.required]],
+      exitDate: [null]
+    })
    }
+
+   ngOnInit(): void {
+    const entryDateControl = this.productForm.get('entryDate');
+    const exitDateControl = this.productForm.get('exitDate');
+  
+    if (entryDateControl && exitDateControl) {
+      exitDateControl.setValidators([
+        this.compareDatesValidator(entryDateControl)
+      ]);
+      exitDateControl.updateValueAndValidity();
+    }
+  }
 
   productQuantityValidator(control: AbstractControl): { [key: string]: any } | null {
     const value = control.value;
@@ -51,7 +68,32 @@ export class ProductRegisterComponent {
     return null;
   }
 
+  compareDatesValidator(entryDateControl: AbstractControl): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const exitDate = control.value;
+      const entryDate = entryDateControl.value;
+
+      if (exitDate && entryDate && new Date(exitDate) <= new Date(entryDate)) {
+        return { 'invalidDateComparison': true };
+      }
+      return null;
+    };
+  }
+
   onSubmit() {
-    
+    const product: Product = {
+      name: this.productForm.get('productName')?.value,
+      description: this.productForm.get('productDescription')?.value,
+      quantity: this.productForm.get('productQuantity')?.value,
+      category: this.productForm.get('productCategory')?.value,
+      entryDate: this.productForm.get('entryDate')?.value,
+      exitDate: this.productForm.get('exitDate')?.value
+    }
+
+    if(this.productForm.valid){
+      this.dbService.postProduct(product);
+      this.productForm.reset();
+      this.router.navigate(['/stockflow/produtos/listar']);
+    }
   }
 }
